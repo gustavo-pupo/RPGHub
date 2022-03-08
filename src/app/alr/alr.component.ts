c881589d3237246327403cc0195d69a925d68759
gustavo-pupo <gusfepupo@gmail.com>
2022-03-08 03:06:55 -0300

    parte lógica iniciada

--- src/app/alr/alr.component.ts
+++ src/app/alr/alr.component.ts
@@ -1,4 +1,3 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alr.component.css']
 })
 export class ALRComponent implements OnInit {
+
+  nomeALR:string = "";
+  vidaALR:number = parseInt("");
+  manaALR:number = parseInt("");
+  sanidadeALR:number = parseInt("");
+  lutarALR:number = parseInt("");
+  armasPesadasALR:number = parseInt("");
+  forBruALR:number = parseInt("");
+  esquivaALR:number = parseInt("");
+  bloqueioALR:number = parseInt("");
+  counterALR:number = parseInt("");
+  armasLevesALR:number = parseInt("");
+  armasDeFogoALR:number = parseInt("");
+  precisaoALR:number = parseInt("");
+  findALR:number = parseInt("");
+  escutarALR:number = parseInt("");
+  intuitionALR:number = parseInt("");
+  resFisicaALR:number = parseInt("");
+  resMentalALR:number = parseInt("");
+  resEspiritualALR:number = parseInt("");
+  firstAidALR:number = parseInt("");
+  medForenseALR:number = parseInt("");
+  medicinaALR:number = parseInt("");
+  velocidadeALR:number = parseInt("");
+  escaladaALR:number = parseInt("");
+  acrobaciaALR:number = parseInt("");
+  magiaALR:number = parseInt("");
+  intelectoALR:number = parseInt("");
+  conhecimentoALR:number = parseInt("");
+  persuasaoALR:number = parseInt("");
+  diplomaciaALR:number = parseInt("");
+  intimidacaoALR:number = parseInt("");
 
   sucesso:boolean;
   tierSucesso:string;
@@ -19,19 +50,71 @@ export class ALRComponent implements OnInit {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  salvar(){
+    localStorage.setItem("nomeALR",this.nomeALR);
+    localStorage.setItem("vidaALR",this.vidaALR.toString());
+    localStorage.setItem("manaALR",this.manaALR.toString());
+    localStorage.setItem("sanidadeALR",this.sanidadeALR.toString());
+    localStorage.setItem("lutarALR",this.lutarALR.toString());
+    localStorage.setItem("armasPesadasALR",this.armasPesadasALR.toString());
+    localStorage.setItem("forBruALR",this.forBruALR.toString());
+    localStorage.setItem("esquivaALR",this.esquivaALR.toString());
+    localStorage.setItem("bloqueioALR",this.bloqueioALR.toString());
+    localStorage.setItem("counterALR",this.counterALR.toString());
+    localStorage.setItem("armasLevesALR",this.armasLevesALR.toString());
+    localStorage.setItem("armasDeFogoALR",this.armasDeFogoALR.toString());
+    localStorage.setItem("precisaoALR",this.precisaoALR.toString());
+    localStorage.setItem("findALR",this.findALR.toString());
+    localStorage.setItem("escutarALR",this.escutarALR.toString());
+    localStorage.setItem("intuitionALR",this.intuitionALR.toString());
+    localStorage.setItem("resFisicaALR",this.resFisicaALR.toString());
+    localStorage.setItem("resMental",this.resMentalALR.toString());
+    localStorage.setItem("resEspititualALR",this.resEspiritualALR.toString());
+    localStorage.setItem("firstAidALR",this.firstAidALR.toString());
+    localStorage.setItem("medForense",this.medForenseALR.toString());
+    localStorage.setItem("medicinaALR",this.medicinaALR.toString());
+    localStorage.setItem("velocidadeALR",this.velocidadeALR.toString());
+    localStorage.setItem("escaladaALR",this.escaladaALR.toString());
+    localStorage.setItem("acrobaciaALR",this.acrobaciaALR.toString());
+    localStorage.setItem("magiaALR",this.magiaALR.toString());
+    localStorage.setItem("intelectoALR",this.intelectoALR.toString());
+    localStorage.setItem("conhecimentoALR",this.conhecimentoALR.toString());
+    localStorage.setItem("persuasaoALR",this.persuasaoALR.toString());
+    localStorage.setItem("diplomaciaALR",this.diplomaciaALR.toString());
+    localStorage.setItem("intimidacaoALR",this.intimidacaoALR.toString());
+  }
+
+    contador:number = 1;
+    incomoda:string = 'incomodam ';
+
+    elefanteFdp(){
+        while(this.contador > 0){
+            if(this.contador == 1){
+                alert(`${this.contador} elefante incomoda muita gente.`);
+                this.contador++;
+            }else{
+            alert(this.contador + ' elefantes ' + this.incomoda.repeat(this.contador) +'muito mais.');
+            this.contador++;
+            alert(`${this.contador} elefantes incomodam muita gente.`);
+            this.contador++;
+            }
+        }
+    }
+
   calcularSucesso(pericia){
     var dado = this.d100();
-    console.log(dado);
+    if(pericia >= 81){
+      this.elefanteFdp();
+    }
     if(dado <= pericia){
       this.sucesso = true;
     }else{
       this.sucesso = false;
     }
-    return this.sucesso && dado;
+    this.calcularTipoSucesso(this.sucesso, pericia, dado)
   }
 
   calcularTipoSucesso(sucesso, pericia, dado){
-    this.calcularSucesso(pericia)
     if(sucesso == true){
       if(dado > (50/100)*pericia){
         this.tierSucesso = "Normal";
@@ -42,6 +125,9 @@ export class ALRComponent implements OnInit {
       if(dado <= (20/100)*pericia){
         this.tierSucesso = "Extremo";
       }
+      if(dado == 1){
+        this.tierSucesso = "Extremo";
+      }
     }
     if(sucesso == false){
       if(dado == 100){
@@ -50,11 +136,22 @@ export class ALRComponent implements OnInit {
         this.tierSucesso = "Fracasso";
       }
     }
+    console.log(dado);
+    this.dado = dado;
+  }
+
+  testeLutar(){
+    this.calcularSucesso(this.lutarALR);
+  }
+  testeArmasPesadas(){
+    this.calcularSucesso(this.armasPesadasALR);
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    if(localStorage.length > 0){
+      
   }
-
+  }
 }
