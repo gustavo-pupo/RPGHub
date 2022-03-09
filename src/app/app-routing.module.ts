1117191795f698dbf8bf5594b009e57f5f4779cf
gustavo-pupo <gusfepupo@gmail.com>
2022-03-08 23:52:35 -0300

    enviando a pasta certa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 import { USMComponent } from './usm/usm.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
   {path: "usm", component:USMComponent },
   {path: "alr", component:ALRComponent}
 ];
